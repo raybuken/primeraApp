27fc7e6f98cc9c8f9f92a95d20a5724dd37c0870
raybuken <raymundopea00@gmail.com>
2023-09-15 22:27:28 -0400

    add expo routing

--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
+import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function Page() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
